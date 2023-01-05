18c2d28f3469ef324bc0d17669165ad3e8753393
Yoko Kawamoto <yokokawamoto@YokonoMacBook-Pro.local>
2023-01-04 21:53:46 -0800

    Setup Catstagram with images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Rating from './components/Rating'
+import Catstagram from './components/Catstagram';
 
 function App() {
   return (
     <div className="App">
       <Rating />
+      <Catstagram />
     </div>
   );
 }
---
+++ src/components/Catstagram.js
@@ -0,0 +1,5 @@
+export default function Catstagram(){
+  return(
+    <h1>Catstagram</h1>
+  )   
+}
