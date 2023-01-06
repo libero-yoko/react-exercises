0826a58c9dcd03cc7523f8b23d01822b21e9cb09
Yoko Kawamoto <yokokawamoto@YokonoMacBook-Pro.local>
2023-01-05 22:45:39 -0800

    Added Outlet to render nested components under root page upon switching menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <li>
-        <Link to="/rating">Rating</Link>
-      </li>
-      <li>
-        <Link to="/catstagram">Catstagram</Link>
-      </li>
-    </div>
+    <>
+      <nav className="App">
+        <li>
+          <Link to="/rating">Rating</Link>
+        </li>
+        <li>
+          <Link to="/catstagram">Catstagram</Link>
+        </li>
+      </nav>
+      <div id="content">
+        <Outlet />
+      </div>
+    </>
   )
 }
 
