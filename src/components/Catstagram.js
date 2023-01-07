e7f8f2527d20e2d36ec41158fb656def6725f5af
Yoko Kawamoto <yokokawamoto@YokonoMacBook-Pro.local>
2023-01-06 17:21:22 -0800

    Added Catstagram page, moved images to public/img

--- src/components/Catstagram.js
+++ src/components/Catstagram.js
@@ -1,3 +1,58 @@
+import { useState } from 'react'
+import { cats } from '../constants/cats'
+
+const styles = {
+  catstagramContaine: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    rowGap: '4rem'
+  },
+  photo: {
+    justifyContent: 'center',
+    width: '500px',
+    maxWidth: '100vw',
+    maxHeight: '100vw',
+    objectFit: 'contain'
+  },
+  like: {
+    padding: '1rem'
+  },
+  orange: {
+    color: 'orange'
+  },
+  gray: {
+    color: 'gray'
+  }
+}
+
 export default function Catstagram() {
-  return <h1>Catstagram</h1>
+  return (
+    <div style={styles.catstagramContainer}>
+      <h1>Catstagram</h1>
+      <div style={styles.container}>
+        {cats.map((cat) => (
+          <div key={cat.id}>
+            <img style={styles.photo} alt={cat.src} src={cat.src} />
+            <Like />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function Like() {
+  const [liked, setLiked] = useState(false)
+  const color = liked ? styles.orange : styles.gray
+  return (
+    <div onClick={() => setLiked(!liked)} style={{ ...color, ...styles.like }}>
+      ★
+    </div>
+  )
 }
---
+++ src/constants/cats.js
@@ -0,0 +1,30 @@
+export const cats = [
+  {
+    id: 1,
+    src: '/img/cats/cat1.jpg'
+  },
+  {
+    id: 2,
+    src: '/img/cats/cat2.jpg'
+  },
+  {
+    id: 3,
+    src: '/img/cats/cat3.jpg'
+  },
+  {
+    id: 4,
+    src: '/img/cats/cat4.jpg'
+  },
+  {
+    id: 5,
+    src: '/img/cats/cat5.jpg'
+  },
+  {
+    id: 6,
+    src: '/img/cats/cat6.jpg'
+  },
+  {
+    id: 7,
+    src: '/img/cats/cat7.jpg'
+  }
+]
