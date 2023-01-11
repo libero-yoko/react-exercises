764105d72ac1a6f9b4d42348d4b7d854d4405c20
Yoko Kawamoto <yokokawamoto@YokonoMacBook-Pro.local>
2023-01-11 09:40:08 -0800

    Show default boxes, validat input in Wordly

--- src/components/Wordly.js
+++ src/components/Wordly.js
@@ -4,6 +4,9 @@ const styles = {
   container: {
     margin: '1em'
   },
+  form: {
+    marginTop: '1em'
+  },
   row: {
     display: 'flex',
     flexDirection: 'row'
@@ -28,34 +31,54 @@ const styles = {
 
 export default function Wordly() {
   const answerWord = 'style'
+  const pattern = /^[A-Za-z]+$/
   const [inputValue, setInputValue] = useState('')
-  const initialArray = ['']
+  const initialArray = new Array(5).fill('-----')
+  const [currentIndex, setCurrentIndex] = useState(0)
   const [inputWords, setInputWords] = useState(initialArray)
+  const [alertMessage, setAlertMessage] = useState(null)
   const inputOnchangeHandler = (event) => {
     setInputValue(event.target.value)
   }
   const onClickSubmitHandler = (event) => {
     event.preventDefault()
+    if (currentIndex > 5) {
+      return
+    }
+    if (
+      inputValue.trim().length > 5 ||
+      inputValue.trim().length < 5 ||
+      !inputValue.match(pattern)
+    ) {
+      setAlertMessage(
+        <>
+          Invalid input. <br /> Pick 5 letter word with only alphabets.
+        </>
+      )
+      return
+    } else {
+      setAlertMessage(null)
+    }
     const newWords = [...inputWords]
-    newWords.push(inputValue)
+    newWords[currentIndex] = inputValue
     setInputWords(newWords)
     setInputValue('')
+    setCurrentIndex(currentIndex + 1)
   }
 
   return (
     <div style={styles.container}>
       {inputWords.map((word, index) => {
-        console.log(word)
         return <Row key={index} inputWord={word} answerWord={answerWord} />
       })}
-
-      <form>
+      {alertMessage}
+      <form style={styles.form}>
         <input
           type="text"
           value={inputValue}
           onChange={inputOnchangeHandler}
         ></input>
-        <button onClick={onClickSubmitHandler}>submit</button>
+        <button onClick={onClickSubmitHandler}>Enter</button>
       </form>
     </div>
   )
@@ -63,7 +86,6 @@ export default function Wordly() {
 
 function Row(props) {
   const { inputWord, answerWord } = props
-  console.log(inputWord, answerWord)
   const charArray = inputWord.split('')
   return (
     <div style={styles.row}>
@@ -85,5 +107,7 @@ function Cell(props) {
     color = styles.gray
   }
 
-  return <div style={{ ...color, ...styles.cell }}>{char}</div>
+  return (
+    <div style={{ ...color, ...styles.cell }}>{char === '-' ? null : char}</div>
+  )
 }
